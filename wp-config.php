4be6147ee44f830b1f6c469faa98c1fa9ac980d0
fdimant <contato@bono.com.br>
2018-01-04 17:49:03 +0000

    configurado wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,23 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'wpuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'bono@2018');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'myaurora.cluster-cmx9lwjftula.us-east-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+define('FS_METHOD', 'direct');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +47,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'h(T8WOrH6]53?sy=IZ!%0d%e-wPQ)|t#FU&|1#}(J0V)0DH7-j53D?nmHwBVfoD4');
+define('SECURE_AUTH_KEY',  '%YkQ&[R+0faf >5<>0}j|_emo#Yv+Mw+}QL)=!M1a1M{{8q+e;c~m:rMQnkB/$@1');
+define('LOGGED_IN_KEY',    '6ciQg3F2,3Qjj_7fGjHDxMpVPsCKey)V&cQM8|mdE_%Y3`L:XXArxgiB+IG^90 H');
+define('NONCE_KEY',        '|Y=HPwb!?h_w716juOc!{{u&Gl6FzG`-%~gLa CHz68{`5K7*xj6VsISWe-K T@G');
+define('AUTH_SALT',        '91MX(Ij?z,3Cj=(:o*B83=#VEU->e;Sox_+ah-RB_Wu:!-!H{y<R-nVYuP(iUv,$');
+define('SECURE_AUTH_SALT', 'Z7`rvs<KP|EmXg+UEYP^|v%~[(^nLXph<6]|ALLy-:v!1P{e|M -ujiP+Q!}/z=d');
+define('LOGGED_IN_SALT',   'HF98Jv{+U}Y7APC}N9fA:H(I-D`[5UlEW2)iVy|H5-Ho?nSRm7:FhC1EVj)*S%Gu');
+define('NONCE_SALT',       'Dwrds~;&QEj4@QmqjS1{Pyp/>FHvb7KH:O?q*-BG;~RQA8wi:UB(|/gi+J@bjeIn');
 
 /**#@-*/
 
